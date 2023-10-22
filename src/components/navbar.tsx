23485d19c598780ab82977215d3d7f0cd01f2812
Tomàs Forné Cappeau <me@tforne.com>
2023-10-22 15:19:33 +0000

    feat: changed style of navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { type ReactElement } from "react";
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "next-i18next";
+import Link from "next/link";
 
 interface NavBarProps {
   links: Array<
@@ -13,13 +14,18 @@ const NavBar: React.FC<NavBarProps> = (props) => {
   const { t } = useTranslation();
 
   return (
-    <nav className="fixed inset-x-0 top-0">
-      <div className="bg-navbar container flex min-h-[80px] items-center justify-between py-2 md:py-4">
+    <nav className="bg-navbar fixed inset-x-0 top-0">
+      <div className="container flex min-h-[80px] items-center justify-between py-2 md:py-4">
         <Image src={props.logo} alt={t("brand")} width={60} height={60} />
         <ul className="flex items-center gap-4">
           {props.links.map((link) => (
             <li key={link.label}>
-              {"component" in link ? link.component : <a href={link.link}>{link.label}</a>}
+              {
+                  "component" in link ? (
+                link.component
+              ) : (
+                <Link href={link.link}>{link.label}</Link>
+              )}
             </li>
           ))}
         </ul>
